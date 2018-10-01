f9c90cf4f76c9b4356ab97df34385b82bc72f040
Aluno Enap <aluno@enap.df>
2018-10-01 19:03:10 -0300

    exercicio atualizado

--- Exercicio aula 4.R
+++ Exercicio aula 4.R
@@ -23,3 +23,20 @@ juizes_drogas_CL <- decisoes %>%
            summarise(tempo_medio = mean(tempo, na.rm=T))
 juizes_drogas_CL         
 write_rds(juizes_drogas_CL,"C:/Users/aluno/Documents/ENAPD6_luciola/juizes_drogas_CL.rds")
+
+decisoes_juiz <- decisoes %>%
+  #select(juiz, n_processo_drogas,n_processosn_drogas,total_processos)
+  filter(is.na(txt_deecisao))%>%
+  mutate(txt_decisao = tolower(txt_decisao),
+         droga = str_detect(txt_decisao,"droga|entorpecente"),
+         droga= case_when(
+           droga==TRUE~"droga"
+           droga==FALSE~"N_droga"
+         ))%>%
+group_by(juiz, droga)%>%
+  summarise(qtd_decisoes=n(),)
+muate(total=droga+n_droga,
+      proporcao=droga/total)%>%
+  arrange(-proporcao)
+
+decisoes_juiz
